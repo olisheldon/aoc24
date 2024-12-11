23db2567a93dc457af5ad25b0342743c45fe0569
olisheldon <oliversheldon00@gmail.com>
2024-12-11 20:07:11 +0000

    Tried to implement day11 part 2 using linked list, too slow

--- python/day11.py
+++ python/day11.py
@@ -1,37 +1,66 @@
 import sys
 
+class Node:
+
+    def __init__(self, val = 0):
+        self.next = None
+        self.val = val
+    
+def create_stones(list_stones):
+    curr = dummy = Node()
+    for stone in list_stones[:]:
+        curr.next = Node(stone)
+        curr = curr.next
+    return dummy.next
+
 # RULES
 
 # 1. If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
 # 2. If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
 # 3. If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
 
-def blink(stones):
-    i = 0
-    while i < len(stones):
-        stones_i_str = str(stones[i])
-        if stones[i] == 0:
-            stones[i] = 1
-        elif len(stones_i_str) % 2 == 0:
-            stones[i] = int(stones_i_str[:len(stones_i_str) // 2])
-            stones.insert(i, int(stones_i_str[len(stones_i_str) // 2:]))
-            i += 1
-        else:
-            stones[i] *= 2024
-        i += 1
-    return stones
+def print_ll_stones(head):
+    curr = head
+    s = []
+    while curr:
+        s.append(str(curr.val))
+        curr = curr.next
+    print(", ".join(s))
 
 def part1():
-    stones = lines[:]
-    for _ in range(25):
-        stones = blink(stones)
-    return len(stones)
+    stones = create_stones(lines[:])
+    for i in range(25):
+        stones = blink_linked_list(stones)
+
+    # len
+    i = 0
+    while stones:
+        i += 1
+        stones = stones.next
+    return i
+
+def blink_linked_list(head):
+    curr = head
+    while curr:
+        curr_val_str = str(curr.val)
+        if curr.val == 0:
+            curr.val = 1
+        elif len(curr_val_str) % 2 == 0:
+            curr.val = int(curr_val_str[:len(curr_val_str) // 2])
+            next_val = int(curr_val_str[len(curr_val_str) // 2:])
+            old_next = curr.next
+            curr.next = Node(next_val)
+            curr.next.next = old_next
+            curr = curr.next
+        else:
+            curr.val *= 2024
+        curr = curr.next
+    return head
 
 def part2():
-    stones = lines[:]
+    stones = create_stones(lines[:])
     for i in range(75):
-        print(i)
-        stones = blink(stones)
+        stones = blink_linked_list(stones)
     return len(stones)
 
 def parse():
@@ -42,7 +71,7 @@ def parse():
 
 if __name__ == "__main__":
 
-    filename = sys.argv[1] if len(sys.argv) > 1 else "../data/day11.txt"
+    filename = sys.argv[1] if len(sys.argv) > 1 else "../data/day11.test.txt"
     lines = parse()
 
     print(part1())
