fcfe6e2b501e55a264a259fe84d3c9c824b1a750
ojsheld <oliver.sheldon@bt.com>
2024-12-16 10:49:58 +0000

    Tried to complete day 16 part 2 in Python

--- python/day16.py
+++ python/day16.py
@@ -29,7 +29,7 @@ class Direction(Enum):
     
 ROTATIONS = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
-def show_history(grid, history):
+def show_history(grid, history, show_path=True):
     grid = grid.copy()
     history_dict = {}
     for r, c, direction in history:
@@ -37,8 +37,11 @@ def show_history(grid, history):
     for r, row in enumerate(grid):
         for c, elem in enumerate(row):
             if (r, c) in history_dict:
-                grid[r][c] = Direction.to_str(Direction(direction))
-                
+                if show_path:
+                    grid[r][c] = Direction.to_str(Direction(direction))
+                else:
+                    grid[r][c] = "O"
+            
     for row in grid:
         print("".join(row))
     print()
@@ -53,12 +56,11 @@ def part1():
         for c in range(COLS):
             if grid[r][c] == START:
                 initial_pos = (r, c)
-    print(f"{initial_pos=}")
+                
     min_heap = [(0, initial_pos[0], initial_pos[1], EAST, [])] # (score, r, c, direction, history)
     visited = set()
     while min_heap:
         score, r, c, direction, history = heapq.heappop(min_heap)
-        # print(score, r, c, direction)
         dr, dc = ROTATIONS[direction]
         if (
             r not in range(ROWS) or
@@ -68,8 +70,7 @@ def part1():
         ):
             continue
         if grid[r][c] == END:
-            show_history(grid, history)
-            return score
+            break
         visited.add((r, c, dr, dc))
         
         # go straight
@@ -79,10 +80,59 @@ def part1():
         heapq.heappush(min_heap, (score + 1000, r, c, (direction + 1) % 4, history + [(r, c, direction)]))
         heapq.heappush(min_heap, (score + 1000, r, c, (direction - 1) % 4, history + [(r, c, direction)]))
     
-    return -1
+    
+    # show_history(grid, history)
+    return score
 
 def part2():
-    pass
+    
+    grid = parse()
+    ROWS, COLS = len(grid), len(grid[0])
+    
+    initial_pos = (-1, -1)
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == START:
+                initial_pos = (r, c)
+    
+    min_score_histories = [] # (r, c, direction)
+    min_score = 0
+    
+    min_heap = [(0, initial_pos[0], initial_pos[1], EAST, [])] # (score, r, c, direction, history)
+    visited = set()
+    while min_heap:
+        score, r, c, direction, history = heapq.heappop(min_heap)
+        dr, dc = ROTATIONS[direction]
+        if (
+            r not in range(ROWS) or
+            c not in range(COLS) or
+            grid[r][c] == WALL or
+            (r, c, dr, dc) in visited
+        ):
+            continue
+        if grid[r][c] == END:
+            print(f"{score=}")
+            if not min_score:
+                min_score = score
+                min_score_histories.append(history + [(r, c, direction)])
+            elif score == min_score:
+                min_score_histories.append(history + [(r, c, direction)])
+            else:
+                break
+        visited.add((r, c, dr, dc))
+        
+        # go straight
+        heapq.heappush(min_heap, (score + 1, r + dr, c + dc, direction, history + [(r, c, direction)]))
+        
+        # try turning
+        heapq.heappush(min_heap, (score + 1000, r, c, (direction + 1) % 4, history + [(r, c, direction)]))
+        heapq.heappush(min_heap, (score + 1000, r, c, (direction - 1) % 4, history + [(r, c, direction)]))
+    
+    flattened_history = []
+    for history in min_score_histories:
+        flattened_history += history
+    show_history(grid, flattened_history, False)
+    return len(set((r, c) for history in min_score_histories for r, c, direction in history))
 
 def parse():
     with open(filename) as f:
