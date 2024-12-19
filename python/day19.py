3210969d0624cfe74758f3aef931a5439097a166
ojsheld <oliver.sheldon@bt.com>
2024-12-19 08:57:51 +0000

    Completed day 19 part 1 in Python

---
+++ python/day19.py
@@ -0,0 +1,30 @@
+import sys
+
+def part1():
+    towels, target_towels = parse()
+    count = []
+    for target_towel in target_towels:
+        res = [False] * (len(target_towel) + 1)
+        res[-1] = True
+        for i in range(len(target_towel) - 1, -1, -1):
+            for towel in towels:
+                if i + len(towel) <= len(target_towel) and towel == target_towel[ i : i + len(towel) ]:
+                    res[i] = max(res[i], res[i + len(towel)])
+        count.append(res[0])
+    return sum(count)
+
+def part2():
+    pass
+
+def parse():
+    with open(filename) as f:
+        inp = f.read()
+    towels, target_towels = inp.split("\n\n")
+    return towels.split(", "), target_towels.split("\n")
+
+if __name__ == "__main__":
+
+    filename = sys.argv[1] if len(sys.argv) > 1 else "../data/day19.txt"
+
+    print(part1())
+    print(part2())
