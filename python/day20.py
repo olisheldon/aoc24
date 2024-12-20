cb358b45fa7abffe854107248b51f2b56b98c739
ojsheld <oliver.sheldon@bt.com>
2024-12-20 11:55:54 +0000

    Completed day 20 part 1 in Python

--- python/day20.py
+++ python/day20.py
@@ -1,19 +1,81 @@
 import sys
 from pathlib import Path
+import heapq
+from collections import Counter
+
+
+START = "S"
+END = "E"
+WALL = "#"
+TRACK = "."
+
+TIME_SAVED = 100
+
+def min_path(grid, cheat_coord, start, end):
+    
+    ROWS, COLS = len(grid), len(grid[0])
+    NEIGHBOURS = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    visited = set()
+        
+    min_heap = [(0, start[0], start[1])] # (steps, r, c)
+    while min_heap:
+        steps, r, c = heapq.heappop(min_heap)
+        if (
+            r not in range(ROWS) or
+            c not in range(COLS) or
+            (r, c) in visited or
+            grid[r][c] == WALL
+        ):
+            continue
+        if (r, c) == end:
+            return steps
+        visited.add((r, c))
+        
+        for dr, dc in NEIGHBOURS:
+            if (r + dr, c + dc) == cheat_coord:
+                heapq.heappush(min_heap, (steps + 2, r + 2 * dr, c + 2 * dc))
+            else:
+                heapq.heappush(min_heap, (steps + 1, r + dr, c + dc))
+    return -1
 
 def part1():
-    return "INCOMPLETE"
+    grid =  parse()
+    ROWS, COLS = len(grid), len(grid[0])
+    
+    start = (-1, -1)
+    end = (-1, -1)
+    
+    for r, row in enumerate(grid):
+        for c, elem in enumerate(row):
+            if elem == START:
+                start = (r, c)
+            elif elem == END:
+                end = (r, c)
+    assert start != (-1, -1) and end != (-1, -1)
+    
+    default_time = min_path(grid, (-1, -1), start, end)
+    cheat_times = [min_path(grid, (r, c), start, end) for r in range(ROWS) for c in range(COLS)]
+    times_in_range = list(filter(lambda x: x in range(default_time - TIME_SAVED + 1), cheat_times))
 
+    # counter = Counter(list(map(lambda x: default_time - x, filter(lambda x: x in range(default_time - TIME_SAVED + 1 ), cheat_times))))
+    # for time_saved, count in sorted(counter.items()):
+    #     print(f"There are {count} cheats that save {time_saved} picoseconds.")
+    return len(times_in_range)
+    
 def part2():
     return "INCOMPLETE"
 
 def parse():
     with open(filename) as f:
         inp = f.read()
+    grid = []
+    for line in inp.split("\n"):
+        grid.append(list(line))
+    return grid
 
 if __name__ == "__main__":
 
-    filename = sys.argv[1] if len(sys.argv) > 1 else f"../data/${Path(__file__).stem}.txt"
+    filename = sys.argv[1] if len(sys.argv) > 1 else f"../data/{Path(__file__).stem}.txt"
 
     print("part1=" + str(part1()))
     print("part2=" + str(part2()))
