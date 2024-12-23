6c1b190aa564333d39980d773670389ec6f2b8ae
ojsheld <oliver.sheldon@bt.com>
2024-12-23 10:57:10 +0000

    Completed day 23 part 1 in Python

--- python/day23.py
+++ python/day23.py
@@ -7,28 +7,29 @@ def part1():
     connections = parse()
     computers = set(chain(*connections))
     
-    adj = defaultdict(list)
+    adj = defaultdict(set)
     for u, v in connections:
-        adj[u].append(v)
-        adj[v].append(u)
-    
-    def dfs(u, prev=None, history=0):
-        if u in visited:
-            return history == 3
-        visited.add(u)
-        
-        for v in adj[u]:
-            if v != prev and dfs(v, u, history + 1):
-                return True
-        return False
+        adj[u].add(v)
+        adj[v].add(u)
 
-    three_computers = 0
-    for u in computers:
-        visited = set()
-        if dfs(u):
-            three_computers += 1
-    return three_computers
-            
+    seen = set()
+    for first_computer in computers:
+        for second_computer in adj[first_computer]:
+            for third_computer in adj[second_computer]:
+                three_computers = frozenset([first_computer, second_computer, third_computer])
+                if len(three_computers) != 3:
+                    continue
+                if (
+                    first_computer in adj[second_computer] and
+                    first_computer in adj[third_computer] and
+                    second_computer in adj[third_computer]
+                ):
+                    seen.add(three_computers)
+                    
+    def contains_element_starting_with_t(iter):
+        return any(element.startswith('t') for element in iter)
+    return len(list(filter(lambda x: contains_element_starting_with_t(x), seen)))
+
 def part2():
     return "INCOMPLETE"
 
@@ -40,7 +41,7 @@ def parse():
 
 if __name__ == "__main__":
 
-    filename = sys.argv[1] if len(sys.argv) > 1 else f"../data/{Path(__file__).stem}.test.txt"
+    filename = sys.argv[1] if len(sys.argv) > 1 else f"../data/{Path(__file__).stem}.txt"
 
     print("part1=" + str(part1()))
     print("part2=" + str(part2()))
