caa1abba20a269ed8dcf1fa70afd1df995e7bf79
ojsheld <oliver.sheldon@bt.com>
2024-12-24 09:11:11 +0000

    Completed day 21 part 2 in Python

--- python/day21.py
+++ python/day21.py
@@ -2,10 +2,22 @@ import sys
 from pathlib import Path
 from collections import deque
 from itertools import product
+from functools import cache
 
 NEIGHBOURS = ((1, 0, "v"), (-1, 0, "^"), (0, 1, ">"), (0, -1, "<"))
+NUM_KEYPAD = [
+    ["7", "8", "9"],
+    ["4", "5", "6"],
+    ["1", "2", "3"],
+    [None, "0", "A"]
+]
+DIR_KEYPAD = [
+    [None, "^", "A"],
+    ["<", "v", ">"]
+]
 
-def solve(string, keypad):
+def compute_seqs(keypad):
+    
     ROWS, COLS = len(keypad), len(keypad[0])
     
     pos = {}
@@ -13,6 +25,7 @@ def solve(string, keypad):
         for c in range(COLS):
             if keypad[r][c] is not None:
                 pos[keypad[r][c]] = (r, c)
+    
     
     # map of every button pair to sequence to move between
     seqs = {}
@@ -46,31 +59,23 @@ def solve(string, keypad):
                     continue
                 break
             seqs[(u, v)] = possibilities
-    
+    return seqs
+
+def solve(string, seqs):
     options = [seqs[(u, v)] for u, v in zip("A" + string, string)]
     return ["".join(u) for u in product(*options)]
 
 def part1():
-    num_keypad = [
-        ["7", "8", "9"],
-        ["4", "5", "6"],
-        ["1", "2", "3"],
-        [None, "0", "A"]
-    ]
-    dir_keypad = [
-        [None, "^", "A"],
-        ["<", "v", ">"]
-    ]
     
     codes = parse()
     total = 0
     for code in codes:
-        robot1 = solve(code, num_keypad)
+        robot1 = solve(code, compute_seqs(NUM_KEYPAD))
         next = robot1
         for _ in range(2):
             possible_next = []
             for seq in next:
-                possible_next += solve(seq, dir_keypad)
+                possible_next += solve(seq, compute_seqs(DIR_KEYPAD))
             minlen = min(map(len, possible_next))
             next = list(filter(lambda u: len(u) == minlen, possible_next))
         length = len(next[0])
@@ -78,8 +83,30 @@ def part1():
         total += complexity
     return total
 
+
 def part2():
-    return "INCOMPLETE"
+    
+    dir_seqs = compute_seqs(DIR_KEYPAD)
+    dir_lens = {key : len(value[0]) for key, value in dir_seqs.items()}
+    num_seqs = compute_seqs(NUM_KEYPAD)
+    
+    @cache
+    def compute_length(seq, depth=25):
+        if depth == 1:
+            return sum(dir_lens[(u, v)] for u, v in zip("A" + seq, seq))
+        length = 0
+        for x, y in zip("A" + seq, seq):
+            length += min(compute_length(subseq, depth -1) for subseq in dir_seqs[(x, y)])
+        return length
+    
+    codes = parse()
+    total = 0
+    for code in codes:
+        inputs = solve(code, num_seqs)
+        optimal_length = min(map(compute_length, inputs))
+        total += optimal_length * int(code[:-1])
+    return total
+
 
 def parse():
     with open(filename) as f:
